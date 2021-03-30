725c3e7f32cd25d1668c6cfda8d9f17391ba8be7
nickmwangemi <nickmwangemi@gmail.com>
2021-03-30 10:31:32 +0300

    Implement Class-Based views and Hyperlinked APIs

--- games/urls.py
+++ games/urls.py
@@ -1,9 +1,17 @@
+from games.models import GameCategory
 from django.urls import path
 from games import views
 
-app_name = 'games'
+
 
 urlpatterns = [
-    path('games/', views.game_list, name='game_list'),
-    path('games/<int:pk>/', views.game_detail, name='game_detail'),
+    path('game-categories/', views.GameCategoryList.as_view(), name=views.GameCategoryList.name),
+    path('game-categories/<int:pk>/', views.GameCategoryDetail.as_view(), name=views.GameCategoryDetail.name),
+    path('games/', views.GameList.as_view(), name=views.GameList.name),
+    path('games/<int:pk>/', views.GameDetail.as_view(), name=views.GameDetail.name),
+    path('players/', views.PlayerList.as_view(), name=views.PlayerList.name),
+    path('players/<int:pk>/', views.PlayerDetail.as_view(), name=views.PlayerDetail.name),
+    path('player-scores/', views.PlayerScoreList.as_view(), name=views.PlayerScoreList.name),
+    path('player-scores/<int:pk>/', views.PlayerScoreDetail.as_view(), name=views.PlayerScoreDetail.name),
+    path('', views.ApiRoot.as_view(), name=views.ApiRoot.name),
 ]
